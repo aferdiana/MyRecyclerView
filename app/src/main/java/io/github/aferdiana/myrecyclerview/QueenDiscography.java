acc0f4ec8299c832aa83f76dd18754c9905efd0b
aferdiana <ferdian.beno99@gmail.com>
2019-05-29 14:34:51 +0700

    Mengembangkan Fitur ListView

---
+++ app/src/main/java/io/github/aferdiana/myrecyclerview/QueenDiscography.java
@@ -0,0 +1,29 @@
+package io.github.aferdiana.myrecyclerview;
+
+public class QueenDiscography {
+    private String title, albumDetail, cover;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAlbumDetail() {
+        return albumDetail;
+    }
+
+    public void setAlbumDetail(String albumDetail) {
+        this.albumDetail = albumDetail;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+}
